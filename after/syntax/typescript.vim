342381fd79c53dbedffb84571ab608fad06340ea
aonemd <aonemdsaleh@gmail.com>
2021-03-16 20:23:29 +0200

    Add conceal support for TypeScript

---
+++ after/syntax/typescript.vim
@@ -0,0 +1,2 @@
+call vimlambdify#lambdify("Statement", "typescriptFuncImpl", "function")
+call vimlambdify#lambdify_match("Statement", "typescriptArrowFunc ", "\"=>\"")
---
+++ tests/tester.ts
@@ -0,0 +1,14 @@
+var a = [
+  "Hydrogen",
+  "Helium",
+  "Lithium",
+  "Beryl­lium"
+];
+
+var a2 = a.map(function(s){ return s.length });
+
+var a3 = a.map( s => s.length );
+
+const hello = (msg: string = '') => {
+  console.log("Hello,", msg);
+}
